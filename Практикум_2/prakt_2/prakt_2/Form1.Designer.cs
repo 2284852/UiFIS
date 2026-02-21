e765704e945f18bc42cc237d3bf1af3d6ed39c6b
Kulikow Andrey <proi70802@gmail.com>
2026-02-21 11:30:55 +0300

    Add files via upload

---
+++ Практикум_2/prakt_2/prakt_2/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace prakt_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.txtInputWord = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pnlLetters = new System.Windows.Forms.Panel();
+            this.lblAnswerLabel = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTitle.Location = new System.Drawing.Point(150, 10);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(200, 30);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Поле чудес";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Location = new System.Drawing.Point(20, 60);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(97, 13);
+            this.lblPrompt.TabIndex = 1;
+            this.lblPrompt.Text = "Введите слово:";
+            // 
+            // txtInputWord
+            // 
+            this.txtInputWord.Location = new System.Drawing.Point(130, 57);
+            this.txtInputWord.Name = "txtInputWord";
+            this.txtInputWord.Size = new System.Drawing.Size(200, 20);
+            this.txtInputWord.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(340, 55);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Начать игру";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pnlLetters
+            // 
+            this.pnlLetters.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlLetters.Location = new System.Drawing.Point(20, 100);
+            this.pnlLetters.Name = "pnlLetters";
+            this.pnlLetters.Size = new System.Drawing.Size(420, 150);
+            this.pnlLetters.TabIndex = 4;
+            // 
+            // lblAnswerLabel
+            // 
+            this.lblAnswerLabel.AutoSize = true;
+            this.lblAnswerLabel.Location = new System.Drawing.Point(20, 270);
+            this.lblAnswerLabel.Name = "lblAnswerLabel";
+            this.lblAnswerLabel.Size = new System.Drawing.Size(70, 13);
+            this.lblAnswerLabel.TabIndex = 5;
+            this.lblAnswerLabel.Text = "Ваш ответ:";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAnswer.Location = new System.Drawing.Point(130, 270);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(300, 20);
+            this.lblAnswer.TabIndex = 6;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Enabled = false;
+            this.btnCheck.Location = new System.Drawing.Point(20, 300);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(100, 30);
+            this.btnCheck.TabIndex = 7;
+            this.btnCheck.Text = "Проверить";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Enabled = false;
+            this.btnUndo.Location = new System.Drawing.Point(130, 300);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(100, 30);
+            this.btnUndo.TabIndex = 8;
+            this.btnUndo.Text = "Отмена";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(240, 300);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(100, 30);
+            this.btnNewGame.TabIndex = 9;
+            this.btnNewGame.Text = "Новая игра";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblStatus.Location = new System.Drawing.Point(20, 340);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(420, 30);
+            this.lblStatus.TabIndex = 10;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 380);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblAnswerLabel);
+            this.Controls.Add(this.pnlLetters);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtInputWord);
+            this.Controls.Add(this.lblPrompt);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Поле чудес";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.TextBox txtInputWord;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel pnlLetters;
+        private System.Windows.Forms.Label lblAnswerLabel;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Практикум_2/prakt_2/prakt_2/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace prakt_2
+{
+    public partial class Form1 : Form
+    {
+        private string originalWord = "";
+        private List<Button> letterButtons = new List<Button>();
+        private string currentAnswer = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string input = txtInputWord.Text.Trim();
+            if (string.IsNullOrEmpty(input) || !input.All(char.IsLetter))
+            {
+                MessageBox.Show("Введите корректное слово (только буквы).", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            originalWord = input.ToUpper();
+            currentAnswer = "";
+            lblAnswer.Text = "";
+            lblStatus.Text = "";
+            lblStatus.BackColor = System.Drawing.Color.Transparent;
+
+            foreach (var btn in letterButtons)
+                pnlLetters.Controls.Remove(btn);
+            letterButtons.Clear();
+
+            var shuffled = ShuffleString(originalWord).ToCharArray();
+            int x = 10, y = 10;
+            const int btnSize = 40;
+            const int spacing = 10;
+
+            for (int i = 0; i < shuffled.Length; i++)
+            {
+                var btn = new Button
+                {
+                    Text = shuffled[i].ToString(),
+                    Size = new System.Drawing.Size(btnSize, btnSize),
+                    Location = new System.Drawing.Point(x, y),
+                    Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold),
+                    Tag = shuffled[i]
+                };
+                btn.Click += LetterButton_Click;
+                pnlLetters.Controls.Add(btn);
+                letterButtons.Add(btn);
+
+                x += btnSize + spacing;
+                if (x > pnlLetters.Width - btnSize)
+                {
+                    x = 10;
+                    y += btnSize + spacing;
+                }
+            }
+
+            btnCheck.Enabled = true;
+            btnUndo.Enabled = true;
+        }
+
+        private string ShuffleString(string word)
+        {
+            var random = new Random();
+            var chars = word.ToCharArray();
+            for (int i = chars.Length - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                (chars[i], chars[j]) = (chars[j], chars[i]);
+            }
+            return new string(chars);
+        }
+
+        private void LetterButton_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn != null)
+            {
+                currentAnswer += btn.Text;
+                lblAnswer.Text = currentAnswer;
+            }
+        }
+
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+            if (currentAnswer.Length > 0)
+            {
+                currentAnswer = currentAnswer.Substring(0, currentAnswer.Length - 1);
+                lblAnswer.Text = currentAnswer;
+            }
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            if (currentAnswer == originalWord)
+            {
+                lblStatus.Text = "✅ Верно! Вы угадали слово!";
+                lblStatus.BackColor = System.Drawing.Color.LightGreen;
+            }
+            else
+            {
+                lblStatus.Text = "❌ Неверно. Попробуйте ещё!";
+                lblStatus.BackColor = System.Drawing.Color.LightPink;
+            }
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            txtInputWord.Clear();
+            lblAnswer.Text = "";
+            lblStatus.Text = "";
+            lblStatus.BackColor = System.Drawing.Color.Transparent;
+            currentAnswer = "";
+            originalWord = "";
+
+            foreach (var btn in letterButtons)
+                pnlLetters.Controls.Remove(btn);
+            letterButtons.Clear();
+
+            btnCheck.Enabled = false;
+            btnUndo.Enabled = false;
+        }
+    }
+}
