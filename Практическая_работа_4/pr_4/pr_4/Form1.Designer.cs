7127d10ae3b16fca7598f03a7bd6996c3b599552
Kulikow Andrey <proi70802@gmail.com>
2026-02-21 12:07:49 +0300

    Add files via upload

---
+++ Практическая_работа_4/pr_4/pr_4/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace pr_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        private void InitializeComponent()
+        {
+            this.lblLambda = new System.Windows.Forms.Label();
+            this.txtLambda = new System.Windows.Forms.TextBox();
+            this.lblMu = new System.Windows.Forms.Label();
+            this.txtMu = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtEquations = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblKp = new System.Windows.Forms.Label();
+            this.lblKg = new System.Windows.Forms.Label();
+            this.lblP2Time = new System.Windows.Forms.Label();
+            this.lblP1Time = new System.Windows.Forms.Label();
+            this.lblP2Stationary = new System.Windows.Forms.Label();
+            this.lblP1Stationary = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.chartPanel = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblLambda
+            // 
+            this.lblLambda.AutoSize = true;
+            this.lblLambda.Location = new System.Drawing.Point(12, 15);
+            this.lblLambda.Name = "lblLambda";
+            this.lblLambda.Size = new System.Drawing.Size(166, 13);
+            this.lblLambda.TabIndex = 0;
+            this.lblLambda.Text = "λ (интенсивность отказов, 1/ч):";
+            // 
+            // txtLambda
+            // 
+            this.txtLambda.Location = new System.Drawing.Point(184, 12);
+            this.txtLambda.Name = "txtLambda";
+            this.txtLambda.Size = new System.Drawing.Size(39, 20);
+            this.txtLambda.TabIndex = 1;
+            // 
+            // lblMu
+            // 
+            this.lblMu.AutoSize = true;
+            this.lblMu.Location = new System.Drawing.Point(228, 15);
+            this.lblMu.Name = "lblMu";
+            this.lblMu.Size = new System.Drawing.Size(209, 13);
+            this.lblMu.TabIndex = 2;
+            this.lblMu.Text = "μ (интенсивность восстановления, 1/ч):";
+            // 
+            // txtMu
+            // 
+            this.txtMu.Location = new System.Drawing.Point(435, 12);
+            this.txtMu.Name = "txtMu";
+            this.txtMu.Size = new System.Drawing.Size(45, 20);
+            this.txtMu.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 88);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(459, 28);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Рассчитать систему уравнений Колмогорова и вероятности состояний";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtEquations);
+            this.groupBox1.Location = new System.Drawing.Point(12, 122);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(459, 100);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Система дифференциальных уравнений Колмогорова";
+            // 
+            // txtEquations
+            // 
+            this.txtEquations.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtEquations.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtEquations.Location = new System.Drawing.Point(3, 16);
+            this.txtEquations.Multiline = true;
+            this.txtEquations.Name = "txtEquations";
+            this.txtEquations.ReadOnly = true;
+            this.txtEquations.Size = new System.Drawing.Size(453, 81);
+            this.txtEquations.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblKp);
+            this.groupBox2.Controls.Add(this.lblKg);
+            this.groupBox2.Controls.Add(this.lblP2Time);
+            this.groupBox2.Controls.Add(this.lblP1Time);
+            this.groupBox2.Controls.Add(this.lblP2Stationary);
+            this.groupBox2.Controls.Add(this.lblP1Stationary);
+            this.groupBox2.Location = new System.Drawing.Point(12, 228);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(459, 137);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Результаты расчёта";
+            // 
+            // lblKp
+            // 
+            this.lblKp.AutoSize = true;
+            this.lblKp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblKp.ForeColor = System.Drawing.Color.Red;
+            this.lblKp.Location = new System.Drawing.Point(6, 111);
+            this.lblKp.Name = "lblKp";
+            this.lblKp.Size = new System.Drawing.Size(162, 15);
+            this.lblKp.TabIndex = 5;
+            this.lblKp.Text = "Коэффициент простоя";
+            // 
+            // lblKg
+            // 
+            this.lblKg.AutoSize = true;
+            this.lblKg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblKg.ForeColor = System.Drawing.Color.Green;
+            this.lblKg.Location = new System.Drawing.Point(6, 91);
+            this.lblKg.Name = "lblKg";
+            this.lblKg.Size = new System.Drawing.Size(184, 15);
+            this.lblKg.TabIndex = 4;
+            this.lblKg.Text = "Коэффициент готовности";
+            // 
+            // lblP2Time
+            // 
+            this.lblP2Time.AutoSize = true;
+            this.lblP2Time.Location = new System.Drawing.Point(6, 71);
+            this.lblP2Time.Name = "lblP2Time";
+            this.lblP2Time.Size = new System.Drawing.Size(128, 13);
+            this.lblP2Time.TabIndex = 3;
+            this.lblP2Time.Text = "P₂(t) в момент времени:";
+            // 
+            // lblP1Time
+            // 
+            this.lblP1Time.AutoSize = true;
+            this.lblP1Time.Location = new System.Drawing.Point(6, 51);
+            this.lblP1Time.Name = "lblP1Time";
+            this.lblP1Time.Size = new System.Drawing.Size(128, 13);
+            this.lblP1Time.TabIndex = 2;
+            this.lblP1Time.Text = "P₁(t) в момент времени:";
+            // 
+            // lblP2Stationary
+            // 
+            this.lblP2Stationary.AutoSize = true;
+            this.lblP2Stationary.Location = new System.Drawing.Point(6, 36);
+            this.lblP2Stationary.Name = "lblP2Stationary";
+            this.lblP2Stationary.Size = new System.Drawing.Size(134, 13);
+            this.lblP2Stationary.TabIndex = 1;
+            this.lblP2Stationary.Text = "Стационарная P₂ (отказ):";
+            // 
+            // lblP1Stationary
+            // 
+            this.lblP1Stationary.AutoSize = true;
+            this.lblP1Stationary.Location = new System.Drawing.Point(6, 21);
+            this.lblP1Stationary.Name = "lblP1Stationary";
+            this.lblP1Stationary.Size = new System.Drawing.Size(205, 13);
+            this.lblP1Stationary.TabIndex = 0;
+            this.lblP1Stationary.Text = "Стационарная P₁ (работоспособность):";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(12, 45);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(148, 13);
+            this.lblTime.TabIndex = 4;
+            this.lblTime.Text = "Время наблюдения t (часы):";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(166, 42);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(100, 20);
+            this.txtTime.TabIndex = 5;
+            // 
+            // chartPanel
+            // 
+            this.chartPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.chartPanel.Location = new System.Drawing.Point(12, 354);
+            this.chartPanel.Name = "chartPanel";
+            this.chartPanel.Size = new System.Drawing.Size(459, 216);
+            this.chartPanel.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 582);
+            this.Controls.Add(this.chartPanel);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.txtMu);
+            this.Controls.Add(this.lblMu);
+            this.Controls.Add(this.txtLambda);
+            this.Controls.Add(this.lblLambda);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Практическая работа №4 — Надёжность системы (Вариант 8)";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLambda;
+        private System.Windows.Forms.TextBox txtLambda;
+        private System.Windows.Forms.Label lblMu;
+        private System.Windows.Forms.TextBox txtMu;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtEquations;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblP1Stationary;
+        private System.Windows.Forms.Label lblP2Stationary;
+        private System.Windows.Forms.Label lblP1Time;
+        private System.Windows.Forms.Label lblP2Time;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label lblKg;
+        private System.Windows.Forms.Label lblKp;
+        private System.Windows.Forms.Panel chartPanel;
+    }
+}
+
---
+++ Практическая_работа_4/pr_4/pr_4/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace pr_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDefaults();
+        }
+        private void InitializeDefaults()
+        {
+            txtLambda.Text = "0.002";
+            txtMu.Text = "0.05";
+            txtTime.Text = "100";
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CultureInfo invariantCulture = CultureInfo.InvariantCulture;
+
+                double lambda;
+                if (!double.TryParse(txtLambda.Text, NumberStyles.Any, invariantCulture, out lambda))
+                {
+                    MessageBox.Show("Неверный формат числа в поле λ (интенсивность отказов). Используйте точку в качестве десятичного разделителя.",
+                        "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtLambda.Focus();
+                    return;
+                }
+
+                double mu;
+                if (!double.TryParse(txtMu.Text, NumberStyles.Any, invariantCulture, out mu))
+                {
+                    MessageBox.Show("Неверный формат числа в поле μ (интенсивность восстановления). Используйте точку в качестве десятичного разделителя.",
+                        "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtMu.Focus();
+                    return;
+                }
+
+                double time;
+                if (!double.TryParse(txtTime.Text, NumberStyles.Any, invariantCulture, out time))
+                {
+                    MessageBox.Show("Неверный формат числа в поле времени. Используйте точку в качестве десятичного разделителя.",
+                        "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtTime.Focus();
+                    return;
+                }
+
+                if (lambda <= 0 || mu <= 0 || time < 0)
+                {
+                    MessageBox.Show("Значения должны быть положительными (λ > 0, μ > 0, время ≥ 0).",
+                        "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                double p1Stationary = mu / (lambda + mu);
+                double p2Stationary = lambda / (lambda + mu);
+
+                double p1_t = p1Stationary + (1 - p1Stationary) * Math.Exp(-(lambda + mu) * time);
+                double p2_t = 1 - p1_t;
+
+                string equations = $"dP₁(t)/dt = -{lambda:F4}·P₁(t) + {mu:F4}·P₂(t)\n" +
+                                   $"dP₂(t)/dt =  {lambda:F4}·P₁(t) - {mu:F4}·P₂(t)";
+                txtEquations.Text = equations;
+
+                lblP1Stationary.Text = $"P₁ (стационарная) = {p1Stationary:F4}";
+                lblP2Stationary.Text = $"P₂ (стационарная) = {p2Stationary:F4}";
+                lblP1Time.Text = $"P₁({time} ч) = {p1_t:F4}";
+                lblP2Time.Text = $"P₂({time} ч) = {p2_t:F4}";
+                lblKg.Text = $"Коэффициент готовности Kг = {p1Stationary:F4}";
+                lblKp.Text = $"Коэффициент простоя Kп = {p2Stationary:F4}";
+
+                DrawGraph(lambda, mu, time);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DrawGraph(double lambda, double mu, double maxTime)
+        {
+            chartPanel.Controls.Clear();
+
+            PictureBox chart = new PictureBox
+            {
+                Dock = DockStyle.Fill,
+                BackColor = System.Drawing.Color.White,
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };
+
+            int panelWidth = chartPanel.Width > 0 ? chartPanel.Width : 459;
+            int panelHeight = chartPanel.Height > 0 ? chartPanel.Height : 150;
+
+            using (var bmp = new System.Drawing.Bitmap(panelWidth, panelHeight))
+            using (var g = System.Drawing.Graphics.FromImage(bmp))
+            {
+                g.Clear(System.Drawing.Color.White);
+                g.DrawString("График вероятностей состояний во времени",
+                    new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Bold),
+                    System.Drawing.Brushes.Black, 10, 10);
+
+                int margin = 40;
+                int width = bmp.Width - 2 * margin;
+                int height = bmp.Height - 2 * margin - 30;
+
+                g.DrawLine(System.Drawing.Pens.Black, margin, bmp.Height - margin,
+                    bmp.Width - margin, bmp.Height - margin);
+                g.DrawLine(System.Drawing.Pens.Black, margin, margin,
+                    margin, bmp.Height - margin);
+
+                double p1_inf = mu / (lambda + mu);
+                int points = 50;
+
+                for (int i = 0; i < points - 1; i++)
+                {
+                    double t1 = i * maxTime / points;
+                    double t2 = (i + 1) * maxTime / points;
+
+                    double p1_t1 = p1_inf + (1 - p1_inf) * Math.Exp(-(lambda + mu) * t1);
+                    double p1_t2 = p1_inf + (1 - p1_inf) * Math.Exp(-(lambda + mu) * t2);
+
+                    int x1 = margin + (int)(i * width / (double)points);
+                    int x2 = margin + (int)((i + 1) * width / (double)points);
+                    int y1 = bmp.Height - margin - (int)(p1_t1 * height);
+                    int y2 = bmp.Height - margin - (int)(p1_t2 * height);
+
+                    g.DrawLine(new System.Drawing.Pen(System.Drawing.Color.Blue, 2), x1, y1, x2, y2);
+                }
+
+                g.DrawString("P₁(t) — работоспособное состояние",
+                    new System.Drawing.Font("Arial", 7F),
+                    new System.Drawing.SolidBrush(System.Drawing.Color.Blue),
+                    margin, bmp.Height - margin + 10);
+                g.DrawString($"Стационарное значение: {p1_inf:F4}",
+                    new System.Drawing.Font("Arial", 7F, System.Drawing.FontStyle.Italic),
+                    System.Drawing.Brushes.DarkGreen,
+                    margin, bmp.Height - margin + 25);
+
+                chart.Image = new System.Drawing.Bitmap(bmp);
+            }
+
+            chartPanel.Controls.Add(chart);
+        }
+    }
+}
