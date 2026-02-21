28fc4d2d937c05d4fc050d32a4eb7f622906fa5d
Kulikow Andrey <proi70802@gmail.com>
2026-02-21 11:39:04 +0300

    Add files via upload

---
+++ Практикум_3/prakt_3/prakt_3/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace prakt_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxInput = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAcceleration = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInitialVelocity = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.groupBoxResults = new System.Windows.Forms.GroupBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblFinalVelocity = new System.Windows.Forms.Label();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.lblMotionType = new System.Windows.Forms.Label();
+            this.groupBoxGraph = new System.Windows.Forms.GroupBox();
+            this.pictureBoxGraph = new System.Windows.Forms.PictureBox();
+            this.groupBoxInput.SuspendLayout();
+            this.groupBoxResults.SuspendLayout();
+            this.groupBoxGraph.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGraph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxInput
+            // 
+            this.groupBoxInput.Controls.Add(this.label6);
+            this.groupBoxInput.Controls.Add(this.label5);
+            this.groupBoxInput.Controls.Add(this.label4);
+            this.groupBoxInput.Controls.Add(this.label3);
+            this.groupBoxInput.Controls.Add(this.txtTime);
+            this.groupBoxInput.Controls.Add(this.label2);
+            this.groupBoxInput.Controls.Add(this.txtAcceleration);
+            this.groupBoxInput.Controls.Add(this.label1);
+            this.groupBoxInput.Controls.Add(this.txtInitialVelocity);
+            this.groupBoxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBoxInput.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxInput.Name = "groupBoxInput";
+            this.groupBoxInput.Size = new System.Drawing.Size(300, 180);
+            this.groupBoxInput.TabIndex = 0;
+            this.groupBoxInput.TabStop = false;
+            this.groupBoxInput.Text = "Входные данные";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.Green;
+            this.label6.Location = new System.Drawing.Point(18, 151);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Рекомендуется ≥ 1 с";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.Green;
+            this.label5.Location = new System.Drawing.Point(16, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "0 — равномерное движение";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Green;
+            this.label4.Location = new System.Drawing.Point(15, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(292, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Положительное — разгон, отрицательное — торможение";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(15, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Время движения (с):";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtTime.Location = new System.Drawing.Point(162, 132);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(120, 22);
+            this.txtTime.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(15, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ускорение (м/с²):";
+            // 
+            // txtAcceleration
+            // 
+            this.txtAcceleration.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtAcceleration.Location = new System.Drawing.Point(162, 82);
+            this.txtAcceleration.Name = "txtAcceleration";
+            this.txtAcceleration.Size = new System.Drawing.Size(120, 22);
+            this.txtAcceleration.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(15, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Начальная скорость (м/с):";
+            // 
+            // txtInitialVelocity
+            // 
+            this.txtInitialVelocity.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtInitialVelocity.Location = new System.Drawing.Point(196, 32);
+            this.txtInitialVelocity.Name = "txtInitialVelocity";
+            this.txtInitialVelocity.Size = new System.Drawing.Size(86, 22);
+            this.txtInitialVelocity.TabIndex = 0;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCalculate.Location = new System.Drawing.Point(12, 200);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(140, 40);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Рассчитать";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClear.Location = new System.Drawing.Point(172, 200);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(140, 40);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // groupBoxResults
+            // 
+            this.groupBoxResults.Controls.Add(this.lblDescription);
+            this.groupBoxResults.Controls.Add(this.lblFinalVelocity);
+            this.groupBoxResults.Controls.Add(this.lblPath);
+            this.groupBoxResults.Controls.Add(this.lblMotionType);
+            this.groupBoxResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBoxResults.Location = new System.Drawing.Point(12, 304);
+            this.groupBoxResults.Name = "groupBoxResults";
+            this.groupBoxResults.Size = new System.Drawing.Size(449, 129);
+            this.groupBoxResults.TabIndex = 2;
+            this.groupBoxResults.TabStop = false;
+            this.groupBoxResults.Text = "Результаты";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblDescription.Location = new System.Drawing.Point(15, 95);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(437, 15);
+            this.lblDescription.TabIndex = 3;
+            this.lblDescription.Text = "Результаты расчёта появятся здесь после нажатия кнопки \"Рассчитать\".";
+            // 
+            // lblFinalVelocity
+            // 
+            this.lblFinalVelocity.AutoSize = true;
+            this.lblFinalVelocity.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblFinalVelocity.Location = new System.Drawing.Point(15, 65);
+            this.lblFinalVelocity.Name = "lblFinalVelocity";
+            this.lblFinalVelocity.Size = new System.Drawing.Size(147, 16);
+            this.lblFinalVelocity.TabIndex = 2;
+            this.lblFinalVelocity.Text = "Конечная скорость: —";
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblPath.Location = new System.Drawing.Point(15, 45);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(138, 16);
+            this.lblPath.TabIndex = 1;
+            this.lblPath.Text = "Пройденный путь: —";
+            // 
+            // lblMotionType
+            // 
+            this.lblMotionType.AutoSize = true;
+            this.lblMotionType.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblMotionType.Location = new System.Drawing.Point(15, 25);
+            this.lblMotionType.Name = "lblMotionType";
+            this.lblMotionType.Size = new System.Drawing.Size(114, 16);
+            this.lblMotionType.TabIndex = 0;
+            this.lblMotionType.Text = "Тип движения: —";
+            // 
+            // groupBoxGraph
+            // 
+            this.groupBoxGraph.Controls.Add(this.pictureBoxGraph);
+            this.groupBoxGraph.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBoxGraph.Location = new System.Drawing.Point(325, 12);
+            this.groupBoxGraph.Name = "groupBoxGraph";
+            this.groupBoxGraph.Size = new System.Drawing.Size(660, 294);
+            this.groupBoxGraph.TabIndex = 1;
+            this.groupBoxGraph.TabStop = false;
+            this.groupBoxGraph.Text = "График зависимости пути от времени S(t)";
+            // 
+            // pictureBoxGraph
+            // 
+            this.pictureBoxGraph.BackColor = System.Drawing.Color.White;
+            this.pictureBoxGraph.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxGraph.Location = new System.Drawing.Point(15, 25);
+            this.pictureBoxGraph.Name = "pictureBoxGraph";
+            this.pictureBoxGraph.Size = new System.Drawing.Size(630, 261);
+            this.pictureBoxGraph.TabIndex = 0;
+            this.pictureBoxGraph.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 445);
+            this.Controls.Add(this.groupBoxGraph);
+            this.Controls.Add(this.groupBoxResults);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.groupBoxInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Анализатор механического движения";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBoxInput.ResumeLayout(false);
+            this.groupBoxInput.PerformLayout();
+            this.groupBoxResults.ResumeLayout(false);
+            this.groupBoxResults.PerformLayout();
+            this.groupBoxGraph.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGraph)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInitialVelocity;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAcceleration;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.GroupBox groupBoxResults;
+        private System.Windows.Forms.Label lblMotionType;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Label lblFinalVelocity;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.GroupBox groupBoxGraph;
+        private System.Windows.Forms.PictureBox pictureBoxGraph;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Практикум_3/prakt_3/prakt_3/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace prakt_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string DetermineMotionType(double v0, double a, double t)
+        {
+            const double epsilon = 0.001;
+
+            if (Math.Abs(v0) < epsilon && Math.Abs(a) > epsilon && a > 0)
+                return "Движение из состояния покоя";
+
+            if (Math.Abs(v0 + a * t) < epsilon && a < 0)
+                return "Торможение до полной остановки";
+
+            if (Math.Abs(a) < epsilon)
+                return "Равномерное прямолинейное движение";
+
+            if (a > epsilon)
+                return "Равноускоренное движение";
+
+            if (a < -epsilon)
+                return "Равнозамедленное движение";
+
+            return "Неопределённый тип движения";
+        }
+
+        private string GenerateDescription(double v0, double a, double t, double s, double v)
+        {
+            var desc = new System.Text.StringBuilder();
+            desc.AppendLine($"Тело начало движение со скоростью {v0:F2} м/с.");
+
+            if (a > 0)
+                desc.AppendLine($"Ускорение {a:F2} м/с² вызывает равномерный рост скорости.");
+            else if (a < 0)
+                desc.AppendLine($"Замедление {Math.Abs(a):F2} м/с² вызывает равномерное снижение скорости.");
+            else
+                desc.AppendLine("Скорость остаётся постоянной (ускорение отсутствует).");
+
+            desc.AppendLine($"За время {t:F2} с тело прошло путь {s:F2} м.");
+            desc.AppendLine($"Конечная скорость составила {v:F2} м/с.");
+
+            if (v < 0)
+                desc.AppendLine("⚠ Внимание: отрицательная конечная скорость означает изменение направления движения.");
+
+            return desc.ToString();
+        }
+
+
+        private double CalculatePath(double v0, double a, double t)
+        {
+            return v0 * t + 0.5 * a * t * t;
+        }
+
+
+        private double CalculateFinalVelocity(double v0, double a, double t)
+        {
+            return v0 + a * t;
+        }
+
+
+        private void DrawGraph(double v0, double a, double t)
+        {
+            pictureBoxGraph.Image = null;
+
+            if (t <= 0) return;
+
+            Bitmap bmp = new Bitmap(pictureBoxGraph.Width, pictureBoxGraph.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                g.Clear(Color.White);
+
+                int margin = 40;
+                int graphWidth = bmp.Width - 2 * margin;
+                int graphHeight = bmp.Height - 2 * margin;
+                int pointCount = 100;
+
+                double maxS = 0;
+                for (int i = 0; i <= pointCount; i++)
+                {
+                    double time = t * i / pointCount;
+                    double s = CalculatePath(v0, a, time);
+
+                    if (s > maxS) maxS = s;
+                }
+                if (maxS == 0)
+                {
+                    double minS = 0;
+                    for (int i = 0; i <= pointCount; i++)
+                    {
+                        double time = t * i / pointCount;
+                        double s = CalculatePath(v0, a, time);
+                        if (s < minS) minS = s;
+                    }
+                    maxS = Math.Abs(minS) * 1.2; 
+                }
+                else
+                {
+                    maxS *= 1.1; 
+                }
+                if (maxS < 1.0) maxS = 1.0; 
+
+                float scaleX = (float)(graphWidth / t);
+                float scaleY = (float)(graphHeight / maxS); 
+
+                int zeroY = bmp.Height - margin; 
+
+                Pen axisPen = new Pen(Color.Black, 1.5f);
+                Pen gridPen = new Pen(Color.LightGray, 0.5f);
+
+                g.DrawLine(axisPen, margin, zeroY, bmp.Width - margin, zeroY);
+
+                g.DrawLine(axisPen, margin, bmp.Height - margin, margin, margin);
+
+                int divisionsX = 10;
+                for (int i = 1; i <= divisionsX; i++)
+                {
+                    int x = margin + (int)(i * graphWidth / divisionsX);
+                    g.DrawLine(gridPen, x, margin, x, bmp.Height - margin);
+                    g.DrawString((t * i / divisionsX).ToString("F1"), Font, Brushes.Black, x - 12, zeroY + 5);
+                }
+
+                int divisionsY = 8;
+                for (int i = 1; i <= divisionsY; i++)
+                {
+                    double sValue = maxS * i / divisionsY;
+                    int y = zeroY - (int)(sValue * scaleY);
+                    if (y < margin) continue;
+                    g.DrawLine(gridPen, margin, y, bmp.Width - margin, y);
+                    g.DrawString(sValue.ToString("F1"), Font, Brushes.Black, 5, y - 6);
+                }
+
+                g.DrawString("Время t (с)", Font, Brushes.Black, bmp.Width / 2 - 30, bmp.Height - 20);
+                g.DrawString("Путь S (м)", Font, Brushes.Black, 5, bmp.Height / 2 - 10);
+
+                PointF[] points = new PointF[pointCount + 1];
+                for (int i = 0; i <= pointCount; i++)
+                {
+                    double time = t * i / pointCount;
+                    double s = CalculatePath(v0, a, time);
+                    int x = margin + (int)(time * scaleX);
+                    int y = zeroY - (int)(s * scaleY); 
+
+                    if (y < margin) y = margin;
+                    if (y > bmp.Height - margin) y = bmp.Height - margin;
+
+                    points[i] = new PointF(x, y);
+                }
+
+                g.DrawLines(new Pen(Color.Blue, 2), points);
+
+                g.FillEllipse(Brushes.Red, points[0].X - 4, points[0].Y - 4, 8, 8);
+                g.FillEllipse(Brushes.Red, points[pointCount / 2].X - 4, points[pointCount / 2].Y - 4, 8, 8);
+                g.FillEllipse(Brushes.Red, points[pointCount].X - 4, points[pointCount].Y - 4, 8, 8);
+
+                string equation = $"S(t) = {v0:F2}·t + ({a:F2}·t²)/2";
+                g.DrawString(equation, new Font(Font.FontFamily, 9, FontStyle.Bold), Brushes.DarkGreen,
+                    new PointF(margin, bmp.Height - margin + 5));
+            }
+
+            pictureBoxGraph.Image = bmp;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtInitialVelocity.Text, out double v0))
+            {
+                MessageBox.Show("Введите корректное значение начальной скорости!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!double.TryParse(txtAcceleration.Text, out double a))
+            {
+                MessageBox.Show("Введите корректное значение ускорения!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!double.TryParse(txtTime.Text, out double t) || t < 0)
+            {
+                MessageBox.Show("Время должно быть положительным числом!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (t == 0)
+            {
+                lblMotionType.Text = "Тип движения: —";
+                lblPath.Text = "Пройденный путь: 0.00 м";
+                lblFinalVelocity.Text = "Конечная скорость: " + v0.ToString("F2") + " м/с";
+                lblDescription.Text = "Время движения равно нулю. Тело не переместилось.";
+                pictureBoxGraph.Image = null;
+                return;
+            }
+
+            double s = CalculatePath(v0, a, t);
+            double v = CalculateFinalVelocity(v0, a, t);
+            string motionType = DetermineMotionType(v0, a, t);
+            string description = GenerateDescription(v0, a, t, s, v);
+
+            lblMotionType.Text = $"Тип движения: {motionType}";
+            lblPath.Text = $"Пройденный путь: {s:F2} м";
+            lblFinalVelocity.Text = $"Конечная скорость: {v:F2} м/с";
+            lblDescription.Text = description;
+
+            DrawGraph(v0, a, t);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtInitialVelocity.Text = "0";
+            txtAcceleration.Text = "0";
+            txtTime.Text = "5";
+
+            lblMotionType.Text = "Тип движения: —";
+            lblPath.Text = "Пройденный путь: —";
+            lblFinalVelocity.Text = "Конечная скорость: —";
+            lblDescription.Text = "Результаты расчёта появятся здесь после нажатия кнопки \"Рассчитать\".";
+
+            pictureBoxGraph.Image = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtInitialVelocity.Text = "0";
+            txtAcceleration.Text = "0";
+            txtTime.Text = "5";
+            lblMotionType.Text = "Тип движения: —";
+            lblPath.Text = "Пройденный путь: —";
+            lblFinalVelocity.Text = "Конечная скорость: —";
+            lblDescription.Text = "Введите параметры движения и нажмите \"Рассчитать\".";
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
